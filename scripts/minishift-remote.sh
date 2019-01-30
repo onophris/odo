18714b916bbfde7f3cd08707aadd30f3cef82175
syamgk <syamgk01@gmail.com>
2019-01-30 19:50:26 +0530

    change variable from localhost to ip

--- scripts/minishift-remote.sh
+++ scripts/minishift-remote.sh
@@ -3,7 +3,8 @@
 OPENSHIFT_CLIENT_BINARY_URL=${OPENSHIFT_CLIENT_BINARY_URL:-'https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz'}
 MINISHIFT_ARCHIVE_URL=${MINISHIFT_ARCHIVE_URL:-'https://github.com/minishift/minishift/releases/download/v1.30.0/minishift-1.30.0-linux-amd64.tgz'}
 
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+ls ~/.ssh/
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_minishift_rsa
 ls ~/.ssh/
 
 sudo systemctl start docker
@@ -11,7 +12,8 @@ sudo systemctl status docker
 sudo systemctl start firewalld
 sudo systemctl status firewalld
 ssh-keyscan localhost >> ~/.ssh/known_hosts
-cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys 
+# adding minishift key to authorized_keys 
+cat ~/.ssh/id_minishift_rsa.pub >> ~/.ssh/authorized_keys 
 
 ## download oc binaries
 sudo wget $OPENSHIFT_CLIENT_BINARY_URL -O /tmp/openshift-origin-client-tools.tar.gz 2> /dev/null > /dev/null
@@ -29,11 +31,8 @@ sudo tar -xvzf /tmp/minishift.tgz --strip-components=1 -C /usr/local/bin
 minishift version
 
 if [ "$1" = "service-catalog" ]; then
-   MINISHIFT_ENABLE_EXPERIMENTAL=y minishift start --vm-driver generic --remote-ipaddress localhost --remote-ssh-user `whoami` --remote-ssh-key ~/.ssh/id_rsa --extra-clusterup-flags "--enable=*,service-catalog,automation-service-broker" 
-  minishift status
+   MINISHIFT_ENABLE_EXPERIMENTAL=y minishift start --vm-driver generic --remote-ipaddress 127.0.0.1 --remote-ssh-user `whoami` --remote-ssh-key ~/.ssh/id_minishift_rsa --extra-clusterup-flags "--enable=*,service-catalog,automation-service-broker" 
 else
-  minishift start --vm-driver generic --remote-ipaddress localhost --remote-ssh-user `whoami` --remote-ssh-key ~/.ssh/id_rsa
-  echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-  minishift status
+  minishift start --vm-driver generic --remote-ipaddress 127.0.0.1 --remote-ssh-user `whoami` --remote-ssh-key ~/.ssh/id_minishift_rsa
 fi  
-
+minishift status
