3ca007bfe860779f65d13b5a797ddf74c0157a1f
syamgk <syamgk01@gmail.com>
2019-01-31 13:10:53 +0530

    remove debug commands

--- scripts/minishift-remote.sh
+++ scripts/minishift-remote.sh
@@ -3,7 +3,6 @@
 OPENSHIFT_CLIENT_BINARY_URL=${OPENSHIFT_CLIENT_BINARY_URL:-'https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz'}
 MINISHIFT_ARCHIVE_URL=${MINISHIFT_ARCHIVE_URL:-'https://github.com/minishift/minishift/releases/download/v1.30.0/minishift-1.30.0-linux-amd64.tgz'}
 
-ls ~/.ssh/
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_minishift_rsa
 ls ~/.ssh/
 
